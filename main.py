b8c76971920d5e7155a019510a6a523f14dafcc6
PC-01 <gabrielschneider100@gmail.com>
2019-09-11 15:45:48 -0300

    FPS modificado

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import cv2
 
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0)
-    
+    cam.set(cv2.CAP_PROP_FPS, 10)
     while(True):
         ret, frame = cam.read()
         cv2.imshow("Hey", frame)
         #letra 'q'
+        fps = cam.get(cv2.CAP_PROP_FPS)
+        print(fps)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.realise()

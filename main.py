7d3387d2850c26bdd9ba45d9d56c67b1413db4bd
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-09-12 18:36:32 -0300

    modificando...

--- main.py
+++ main.py
@@ -2,19 +2,32 @@
 
 import cv2
 
-def main():
+def setup():
+    print()
+    for i in range(15):
+        print("*",end='\t')
+    print()
+    #print("!CLICK IN 'Q' FOR QUIT OF THE APP!")
+    for i in range((int)(15/3)):
+        print(" ", end='\t')
+    print("!CLICK IN 'Q' FOR QUIT OF THE APP!")
+    for i in range(15):
+        if i == (int)(15/2):
+            print("Begin",end='\t')
+        else:
+            print("*",end='\t')
+    print()
 
+def main():
     name = "Hey"
 
     cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FPS, 90)
+    cam.set(cv2.CAP_PROP_FPS, 180)
     while(True):
         ret, frame = cam.read()
         cv2.imshow(name, frame)
         fps = cam.get(cv2.CAP_PROP_FPS)
-        print(fps, end='\t')
-        #print(cv2.waitKey(1))
-        print()
+        print(fps)
         #letra 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -22,4 +35,5 @@ def main():
     cv2.destroyWindow(name) 
 
 if __name__ == "__main__":
+    setup()
     main()
